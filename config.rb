ac6990e3967b5352583d018736693297c4b43d38
Marcelo Silva <marcelosilva.developer@gmail.com>
2015-01-27 15:15:01 -0300

    Add config.rb, a configuration file

---
+++ config.rb
@@ -0,0 +1,11 @@
+http_path = "http_path"
+css_dir = "css_directory"
+sass_dir = "sass_directory"
+images_dir = "images_directory"
+javascripts_dir = "javascript_directory"
+
+output_style = :expanded
+
+relative_assets = true
+
+line_comments = false
