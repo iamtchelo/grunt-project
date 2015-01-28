e5b44e89f5f4943580ca274821527998a2da8eaf
Marcelo Silva <marcelosilva.developer@gmail.com>
2015-01-27 21:48:24 -0300

    Add config.rb, a simple configuration file

---
+++ grunt/config.rb
@@ -0,0 +1,24 @@
+# Set this to the root of your project when deployed:
+http_path = "http_path"
+
+# Set up your css directory
+css_dir = "css_directory"
+
+# Set up your sass directory
+sass_dir = "sass_directory"
+
+# Set up your images directory
+images_dir = "images_directory"
+
+# Set up your javascripts directory
+javascripts_dir = "javascripts_directory"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
